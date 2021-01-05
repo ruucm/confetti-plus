ce7afab44de8f5d74ead42b69482d28aaedb4ff6
seungmee lee <stone.skipper.ee@gmail.com>
2021-01-05 23:11:26 +0900

    update - check when the confetti ends

--- confetti.framerfx/code/Confetti.tsx
+++ confetti.framerfx/code/Confetti.tsx
@@ -6,258 +6,273 @@ import { getRandomInt } from "./utils"
 import { useInterval } from "./use-interval"
 
 export function Confetti(props) {
-  const canvasRef = useRef(null)
-  const {
-    play,
-    loop,
-    interval,
-    particleNumber,
-    particleColors,
-    emojis,
-    imgs,
-    ...options
-  } = props
-  const particles = []
-  init()
+    const canvasRef = useRef(null)
+    const {
+        play,
+        loop,
+        interval,
+        particleNumber,
+        particleColors,
+        emojis,
+        imgs,
+        ...options
+    } = props
+    const particles = []
+    const activeParticles = []
+    const [activeNum, setActiveNum] = React.useState()
+    var activeCount
 
-  function init() {
-    for (let i = 0; i < particleNumber; i++) {
-      const image = new Image()
+    init()
 
-      if (imgs !== undefined) {
-        image.src = imgs[getRandomInt(0, imgs.length)]
-      }
-      const item = new Particle(
-        particleColors[getRandomInt(0, particleColors.length)],
-        emojis[getRandomInt(0, emojis.length)],
-        image,
-        options
-      )
-      particles[i] = item
+    function init() {
+        for (let i = 0; i < particleNumber; i++) {
+            const image = new Image()
+
+            if (imgs !== undefined) {
+                image.src = imgs[getRandomInt(0, imgs.length)]
+            }
+            const item = new Particle(
+                particleColors[getRandomInt(0, particleColors.length)],
+                emojis[getRandomInt(0, emojis.length)],
+                image,
+                options,
+                props.width,
+                props.height
+            )
+            particles[i] = item
+        }
     }
-  }
 
-  useEffect(() => {
-    const canvas = canvasRef.current
-    const context = canvas.getContext("2d")
-    let animationFrameId
+    useEffect(() => {
+        const canvas = canvasRef.current
+        const context = canvas.getContext("2d")
+        let animationFrameId
 
-    // Our draw came here
-    function render() {
-      context.clearRect(0, 0, context.canvas.width, context.canvas.height)
-      for (let i = 0; i < particleNumber; i++) {
-        const item = particles[i]
-        item.draw(context)
-      }
-      animationFrameId = window.requestAnimationFrame(render)
-    }
-    if (play) render()
+        // Our draw came here
+        function render() {
+            context.clearRect(0, 0, context.canvas.width, context.canvas.height)
+            for (let i = 0; i < particleNumber; i++) {
+                const item = particles[i]
+                item.draw(context)
+                activeParticles[i] = item.active
+            }
+            activeCount = activeParticles.filter(function (s) {
+                return s
+            }).length
+            setActiveNum(activeCount)
 
-    return () => {
-      window.cancelAnimationFrame(animationFrameId)
-    }
-  }, [play])
+            animationFrameId = window.requestAnimationFrame(render)
+        }
+        if (play) render()
 
-  if (loop)
-    useInterval(() => {
-      for (let i = 0; i < particleNumber; i++) {
-        const item = particles[i]
-        item.init()
-      }
-    }, interval * 1000)
+        return () => {
+            window.cancelAnimationFrame(animationFrameId)
+        }
+    }, [play])
 
-  return <canvas ref={canvasRef} {...props} />
+    useEffect(() => {
+        if (activeNum === 0) {
+            console.log("confetti complete")
+        }
+    }, [activeNum])
+
+    if (loop)
+        useInterval(() => {
+            for (let i = 0; i < particleNumber; i++) {
+                const item = particles[i]
+                item.init()
+            }
+        }, interval * 1000)
+
+    return <canvas ref={canvasRef} {...props} />
 }
 
 Confetti.defaultProps = {
-  width: 300,
-  height: 600,
+    width: 300,
+    height: 600,
 }
 
 addPropertyControls(Confetti, {
-  play: {
-    type: ControlType.Boolean,
-  },
-  loop: {
-    type: ControlType.Boolean,
-    defaultValue: false,
-  },
-  interval: {
-    title: "~ Interval",
-    type: ControlType.Number,
-    unit: "sec",
-    displayStepper: true,
-    min: 1,
-    hidden(props) {
-      return props.loop == false
+    play: {
+        type: ControlType.Boolean,
     },
-  },
-  type: {
-    type: ControlType.SegmentedEnum,
-    options: ["shape", "emoji", "image"],
-    optionTitles: ["Shape", "Emoji", "Image"],
-  },
-  shape: {
-    type: ControlType.SegmentedEnum,
-    options: ["rect", "circle", "star"],
-    optionTitles: ["■", "●", "★"],
-    hidden(props) {
-      return props.type !== "shape"
+    loop: {
+        type: ControlType.Boolean,
+        defaultValue: false,
     },
-  },
-  particleNumber: {
-    type: ControlType.Number,
-    defaultValue: 50,
-    min: 0,
-    max: 1000,
-    step: 5,
-    displayStepper: true,
-    hidden(props) {
-      return props.type !== "Shape"
+    interval: {
+        title: "~ Interval",
+        type: ControlType.Number,
+        unit: "sec",
+        displayStepper: true,
+        min: 1,
+        hidden(props) {
+            return props.loop == false
+        },
     },
-  },
-  particleWidth: {
-    type: ControlType.Number,
-    defaultValue: 20,
-    min: 0,
-    max: 1000,
-    unit: "px",
-    step: 5,
-    displayStepper: true,
-    hidden(props) {
-      return props.type !== "shape" || props.shape !== "rect"
+    type: {
+        type: ControlType.SegmentedEnum,
+        options: ["shape", "emoji", "image"],
+        optionTitles: ["Shape", "Emoji", "Image"],
     },
-  },
-  particleHeight: {
-    type: ControlType.Number,
-    defaultValue: 15,
-    min: 0,
-    max: 1000,
-    unit: "px",
-    step: 5,
-    displayStepper: true,
-    hidden(props) {
-      return props.type !== "shape" || props.shape !== "rect"
+    shape: {
+        type: ControlType.SegmentedEnum,
+        options: ["rect", "circle", "star"],
+        optionTitles: ["■", "●", "★"],
+        hidden(props) {
+            return props.type !== "shape"
+        },
     },
-  },
-  particleSize: {
-    type: ControlType.Number,
-    defaultValue: 30,
-    min: 0,
-    max: 1000,
-    unit: "px",
-    step: 5,
-    displayStepper: true,
-    hidden(props) {
-      return props.type === "shape" && props.shape === "rect"
+    particleNumber: {
+        type: ControlType.Number,
+        defaultValue: 50,
+        min: 0,
+        max: 1000,
+        step: 5,
+        displayStepper: true,
     },
-  },
+    particleWidth: {
+        type: ControlType.Number,
+        defaultValue: 20,
+        min: 0,
+        max: 1000,
+        unit: "px",
+        step: 5,
+        displayStepper: true,
+        hidden(props) {
+            return props.type !== "shape" || props.shape !== "rect"
+        },
+    },
+    particleHeight: {
+        type: ControlType.Number,
+        defaultValue: 15,
+        min: 0,
+        max: 1000,
+        unit: "px",
+        step: 5,
+        displayStepper: true,
+        hidden(props) {
+            return props.type !== "shape" || props.shape !== "rect"
+        },
+    },
+    particleSize: {
+        type: ControlType.Number,
+        defaultValue: 30,
+        min: 0,
+        max: 1000,
+        unit: "px",
+        step: 5,
+        displayStepper: true,
+        hidden(props) {
+            return props.type === "shape" && props.shape === "rect"
+        },
+    },
 
-  velocity: {
-    type: ControlType.Number,
-    defaultValue: 10,
-    min: 0,
-    max: 1000,
-    unit: "px",
-    step: 5,
-    displayStepper: true,
-  },
-  friction: {
-    type: ControlType.Number,
-    defaultValue: 0.7,
-    min: 0,
-    max: 1,
-    step: 0.1,
-    displayStepper: true,
-  },
-  gravity: {
-    type: ControlType.Number,
-    defaultValue: 0.02,
-    min: 0,
-    max: 1000,
-    step: 0.01,
-    displayStepper: true,
-  },
-  x: {
-    type: ControlType.Number,
-    defaultValue: 100,
-    min: 0,
-    max: 500,
-    unit: "px",
-    step: 5,
-    displayStepper: true,
-  },
-  y: {
-    type: ControlType.Number,
-    defaultValue: 0,
-    min: 0,
-    max: 1000,
-    unit: "px",
-    step: 5,
-    displayStepper: true,
-  },
-  launchDegree: {
-    type: ControlType.Number,
-    defaultValue: -90,
-    min: -360,
-    max: 360,
-    unit: "deg",
-    step: 90,
-    displayStepper: true,
-  },
-  injectionDegree: {
-    type: ControlType.Number,
-    defaultValue: 120,
-    min: -360,
-    max: 360,
-    unit: "deg",
-    step: 5,
-    displayStepper: true,
-  },
-  wind: {
-    type: ControlType.Number,
-    defaultValue: 0,
-    min: -1,
-    max: 1,
-    step: 0.1,
-    displayStepper: true,
-  },
-  particleColors: {
-    type: ControlType.Array,
-    propertyControl: {
-      type: ControlType.Color,
+    velocity: {
+        type: ControlType.Number,
+        defaultValue: 10,
+        min: 0,
+        max: 1000,
+        unit: "px",
+        step: 5,
+        displayStepper: true,
     },
-    defaultValue: [
-      "#1abc9c",
-      "#2ecc71",
-      "#f1c40f",
-      "#e74c3c",
-      "#c0392b",
-      "#27ae60",
-      "#9b59b6",
-    ],
-    hidden(props) {
-      return props.type !== "shape"
+    friction: {
+        type: ControlType.Number,
+        defaultValue: 0.7,
+        min: 0,
+        max: 1,
+        step: 0.1,
+        displayStepper: true,
     },
-  },
-  emojis: {
-    type: ControlType.Array,
-    propertyControl: {
-      type: ControlType.String,
+    gravity: {
+        type: ControlType.Number,
+        defaultValue: 0.02,
+        min: 0,
+        max: 1000,
+        step: 0.01,
+        displayStepper: true,
     },
-    defaultValue: ["🎉", "🤡", "🎄"],
-    hidden(props) {
-      return props.type !== "emoji"
+    x: {
+        type: ControlType.Number,
+        defaultValue: 100,
+        min: 0,
+        max: 500,
+        unit: "px",
+        step: 5,
+        displayStepper: true,
     },
-  },
-  imgs: {
-    type: ControlType.Array,
-    propertyControl: {
-      type: ControlType.Image,
+    y: {
+        type: ControlType.Number,
+        defaultValue: 0,
+        min: 0,
+        max: 1000,
+        unit: "px",
+        step: 5,
+        displayStepper: true,
     },
-    defaultValue: ["url(./assets/ball.png)"],
-    hidden(props) {
-      return props.type !== "image"
+    launchDegree: {
+        type: ControlType.Number,
+        defaultValue: -90,
+        min: -360,
+        max: 360,
+        unit: "deg",
+        step: 90,
+        displayStepper: true,
     },
-  },
+    injectionDegree: {
+        type: ControlType.Number,
+        defaultValue: 120,
+        min: -360,
+        max: 360,
+        unit: "deg",
+        step: 5,
+        displayStepper: true,
+    },
+    wind: {
+        type: ControlType.Number,
+        defaultValue: 0,
+        min: -1,
+        max: 1,
+        step: 0.1,
+        displayStepper: true,
+    },
+    particleColors: {
+        type: ControlType.Array,
+        propertyControl: {
+            type: ControlType.Color,
+        },
+        defaultValue: [
+            "#1abc9c",
+            "#2ecc71",
+            "#f1c40f",
+            "#e74c3c",
+            "#c0392b",
+            "#27ae60",
+            "#9b59b6",
+        ],
+        hidden(props) {
+            return props.type !== "shape"
+        },
+    },
+    emojis: {
+        type: ControlType.Array,
+        propertyControl: {
+            type: ControlType.String,
+        },
+        defaultValue: ["🎉", "🤡", "🎄"],
+        hidden(props) {
+            return props.type !== "emoji"
+        },
+    },
+    imgs: {
+        type: ControlType.Array,
+        propertyControl: {
+            type: ControlType.Image,
+        },
+        defaultValue: ["url(./assets/ball.png)"],
+        hidden(props) {
+            return props.type !== "image"
+        },
+    },
 })
--- confetti.framerfx/code/particle.js
+++ confetti.framerfx/code/particle.js
@@ -1,119 +1,129 @@
 import { randomRange } from "./utils"
 
 export class Particle {
-  constructor(color, emoji, img, options) {
-    this.color = color
-    this.emoji = emoji
-    this.img = img
-    this.options = options
-
-    this.init()
-  }
-
-  init() {
-    const {
-      type,
-      shape,
-      particleWidth,
-      particleHeight,
-      particleSize,
-      x,
-      y,
-      velocity,
-      friction,
-      gravity,
-      launchDegree,
-      injectionDegree,
-      wind,
-    } = this.options
-
-    this.size = particleSize
-    this.type = type
-    this.shape = shape
-    this.particleWidth = particleWidth
-    this.particleHeight = particleHeight
-
-    this.sizeVari = randomRange(0.8, 1.2) // 크기 다양화
-    const minVx =
-      velocity *
-      Math.cos(((launchDegree - injectionDegree / 2) * Math.PI) / 180)
-    const maxVx =
-      velocity *
-      Math.cos(((launchDegree + injectionDegree / 2) * Math.PI) / 180)
-    const minVy =
-      -velocity *
-      Math.sin(((launchDegree - injectionDegree / 2) * Math.PI) / 180)
-    const maxVy =
-      -velocity *
-      Math.sin(((launchDegree + injectionDegree / 2) * Math.PI) / 180)
-    const f = randomRange(1 - friction, 1)
-    this.vx = randomRange(minVx, maxVx) * f
-    this.vy = randomRange(minVy, maxVy) * f // vy의 끝 값으로 얼마나 높게 파티클이 올라갈지 결정
-
-    this.vr = randomRange(0, 2 * Math.PI) // 돌아가는 회전 속도값
-    this.angle3dvari = randomRange(0, 1)
-    this.gravity = gravity // 중력값
-    this.wind = wind
-    // this.color = '#' + Math.floor(Math.random() * 16777215).toString(16) // 랜덤 컬러
-
-    // 터지는 위치
-    this.x = x
-    this.y = y
-    this.r = 0
-  }
-
-  draw(ctx) {
-    // Set position
-    this.angle3d = 0.5 + Math.sin(this.vy * this.angle3dvari) * 0.5
-
-    this.vy = Math.min(this.vy + this.gravity)
-
-    this.x += this.vx
-    this.vx += this.wind
-
-    this.y += this.vy
-    this.r += this.vr
-
-    ctx.save() // Save x, y, r state (https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/save)
-    ctx.translate(this.x, this.y) // Move context, but no direct movemnents on rectangles.
-    ctx.scale(this.sizeVari, this.sizeVari)
-    ctx.rotate((this.r * Math.PI) / 180)
-
-    // FOR DEFAULT COLOR CONFETTI
-    if (this.type === "shape" && this.shape === "rect") {
-      ctx.scale(this.angle3d, 1) //3d rotation
-      ctx.fillStyle = this.color
-      ctx.fillRect(
-        -this.particleWidth / 2,
-        -this.particleHeight / 2,
-        this.particleWidth,
-        this.particleHeight
-      )
-    } else if (this.type === "shape" && this.shape === "circle") {
-      ctx.scale(this.angle3d, 1) //3d rotation
-      ctx.fillStyle = this.color
-      ctx.beginPath()
-      ctx.arc(0, 0, this.size / 2, 0, 2 * Math.PI)
-      ctx.fill()
+    constructor(color, emoji, img, options, canvasWidth, canvasHeight) {
+        this.color = color
+        this.emoji = emoji
+        this.img = img
+        this.options = options
+        this.canvasWidth = canvasWidth
+        this.canvasHeight = canvasHeight
+        this.init()
     }
 
-    // FOR TEXT/EMOJI CONFETTI
-    else if (this.type === "emoji") {
-      ctx.font = this.size * 2 + "px" + " san-serif"
-      ctx.textAlign = "center"
-      ctx.textBaseline = "middle"
-      ctx.fillText(this.emoji, 0, 0)
-    } else if (this.type === "image" && this.img !== null) {
-      ctx.drawImage(
-        this.img,
-        -(this.size / 2),
-        -((this.size * (this.img.height / this.img.width)) / 2),
-        this.size,
-        this.size * (this.img.height / this.img.width)
-      )
+    init() {
+        const {
+            type,
+            shape,
+            particleWidth,
+            particleHeight,
+            particleSize,
+            x,
+            y,
+            velocity,
+            friction,
+            gravity,
+            launchDegree,
+            injectionDegree,
+            wind,
+        } = this.options
+
+        this.size = particleSize
+        this.type = type
+        this.shape = shape
+        this.particleWidth = particleWidth
+        this.particleHeight = particleHeight
+
+        this.sizeVari = randomRange(0.8, 1.2) // 크기 다양화
+        const minVx =
+            velocity *
+            Math.cos(((launchDegree - injectionDegree / 2) * Math.PI) / 180)
+        const maxVx =
+            velocity *
+            Math.cos(((launchDegree + injectionDegree / 2) * Math.PI) / 180)
+        const minVy =
+            -velocity *
+            Math.sin(((launchDegree - injectionDegree / 2) * Math.PI) / 180)
+        const maxVy =
+            -velocity *
+            Math.sin(((launchDegree + injectionDegree / 2) * Math.PI) / 180)
+        const f = randomRange(1 - friction, 1)
+        this.vx = randomRange(minVx, maxVx) * f
+        this.vy = randomRange(minVy, maxVy) * f // vy의 끝 값으로 얼마나 높게 파티클이 올라갈지 결정
+
+        this.vr = randomRange(0, 2 * Math.PI) // 돌아가는 회전 속도값
+        this.angle3dvari = randomRange(0, 1)
+        this.gravity = gravity // 중력값
+        this.wind = wind
+        // this.color = '#' + Math.floor(Math.random() * 16777215).toString(16) // 랜덤 컬러
+
+        // 터지는 위치
+        this.x = x
+        this.y = y
+        this.r = 0
+
+        this.active = true
     }
 
-    // FOR IMAGE CONFETTI
-    ctx.restore() // Restore x, y, r state
-  }
+    draw(ctx) {
+        // Set position
+        this.angle3d = 0.5 + Math.sin(this.vy * this.angle3dvari) * 0.5
+
+        this.vy = Math.min(this.vy + this.gravity)
+
+        this.x += this.vx
+        this.vx += this.wind
+
+        this.y += this.vy
+        this.r += this.vr
+        if (
+            this.y > this.canvasHeight ||
+            this.x > this.canvasWidth ||
+            this.x < -100 ||
+            this.y < -100
+        ) {
+            this.active = false
+        }
+        ctx.save() // Save x, y, r state (https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/save)
+        ctx.translate(this.x, this.y) // Move context, but no direct movemnents on rectangles.
+        ctx.scale(this.sizeVari, this.sizeVari)
+        ctx.rotate((this.r * Math.PI) / 180)
+
+        // FOR DEFAULT COLOR CONFETTI
+        if (this.type === "shape" && this.shape === "rect") {
+            ctx.scale(this.angle3d, 1) //3d rotation
+            ctx.fillStyle = this.color
+            ctx.fillRect(
+                -this.particleWidth / 2,
+                -this.particleHeight / 2,
+                this.particleWidth,
+                this.particleHeight
+            )
+        } else if (this.type === "shape" && this.shape === "circle") {
+            ctx.scale(this.angle3d, 1) //3d rotation
+            ctx.fillStyle = this.color
+            ctx.beginPath()
+            ctx.arc(0, 0, this.size / 2, 0, 2 * Math.PI)
+            ctx.fill()
+        }
+
+        // FOR TEXT/EMOJI CONFETTI
+        else if (this.type === "emoji") {
+            ctx.font = this.size * 2 + "px" + " san-serif"
+            ctx.textAlign = "center"
+            ctx.textBaseline = "middle"
+            ctx.fillText(this.emoji, 0, 0)
+        } else if (this.type === "image" && this.img !== null) {
+            ctx.drawImage(
+                this.img,
+                -(this.size / 2),
+                -((this.size * (this.img.height / this.img.width)) / 2),
+                this.size,
+                this.size * (this.img.height / this.img.width)
+            )
+        }
+
+        // FOR IMAGE CONFETTI
+        ctx.restore() // Restore x, y, r state
+    }
 }
